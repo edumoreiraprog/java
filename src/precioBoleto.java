6edc90eec911573a14521e4bf7dd9ec98247d41b
Eduardo Moreira <edumoreira462@gmail.com>
2024-04-15 09:41:33 -0300

    Primer commit

---
+++ src/precioBoleto.java
@@ -0,0 +1,10 @@
+// "psvm" : Abreviatura del método "public static void main(String[] args) {}"
+
+/*Calcula el precio de un boleto de viaje, tomando en cuenta el número de kilómetros
+que se van a recorrer, siendo el precio $45 por Km.*/
+
+public class precioBoleto {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/ternario.java
@@ -0,0 +1,6 @@
+public class ternario {
+
+    public static void main(String args) {
+
+    }
+}
