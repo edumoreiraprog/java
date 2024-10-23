4c99b55c5323d662a0d19f99d0be134d85991af3
Eduardo Moreira <edumoreira462@gmail.com>
2024-10-23 11:08:03 -0300

    Se agrego listas enlazadas simple

---
+++ src/ProgramacionII/POO/Ejercicios2110/ListasEnlazadas/Nodo.java
@@ -0,0 +1,23 @@
+package ProgramacionII.POO.Ejercicios2110.ListasEnlazadas;
+
+public class Nodo {
+    private int dato;
+    private Nodo siguiente;
+
+    public Nodo(int dato) {
+        this.dato = dato;
+        this.siguiente = null;
+    }
+
+    public int getDato() {
+        return dato;
+    }
+
+    public Nodo getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo siguiente) {
+        this.siguiente = siguiente;
+    }
+}
